b3fe74e60cb45715b52ebd8d59a0fc1b18b86369
Aditya Parashar <adityaparashar95@gmail.com>
2024-02-29 15:53:55 +0000

    ss

--- src/transformers/generation/test.py
+++ src/transformers/generation/test.py
@@ -2,14 +2,14 @@
 import torch
 import sys
 
-from transformers.models.auto.modeling_auto import AutoModelForCausalLM
+from src.transformers.models.auto.modeling_auto import AutoModelForCausalLM
 # from transformers_local.src.transformers.generation.logits_process import LogitsProcessorList, MinLengthLogitsProcessor
 
 # from transformers.utils.dummy_pt_objects import AutoModelForCausalLM
 
 # print the system path
 print(sys.path)
-from transformers.models.auto.tokenization_auto import AutoTokenizer
+from src.transformers.models.auto.tokenization_auto import AutoTokenizer
 # from transformers import (
 #     AutoTokenizer,
 #     AutoModelForCausalLM,
