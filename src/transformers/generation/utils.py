d69ea998886377ad65d6a26d20e64972dab1fc4c
Aditya Parashar <adityaparashar95@gmail.com>
2024-03-05 13:55:36 -0500

    ..

--- src/transformers/generation/utils.py
+++ src/transformers/generation/utils.py
@@ -2052,8 +2052,10 @@ class GenerationMixin:
                 (all_bucket_maxes * bucket_maxes_gt_codes +
                 bucket_maxes_lte_codes.float() * 1.1).argmin(dim=1)
             )
-            
-            next_tokens = torch.argmax(torch.nn.functional.one_hot(sampled_indices_permed, num_classes=vocab_size)[:, invperm], dim=1)
+            torch.cuda.empty_cache()
+            next_tokens = torch.tensor([perm[i].item() for i in sampled_indices_permed])
+
+            # next_tokens = torch.argmax(torch.nn.functional.one_hot(sampled_indices_permed, num_classes=vocab_size)[:, invperm], dim=1)
 
             
             # Compute new codes for remaining suffix.
