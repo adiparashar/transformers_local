63df075ed0d73ee96900b8d1e918f5d8b0a35a06
Aditya Parashar <adityaparashar95@gmail.com>
2024-04-25 00:47:31 -0400

    changes

--- src/transformers/generation/test.py
+++ src/transformers/generation/test.py
@@ -45,7 +45,7 @@ def test():
         outputs_arith = model.generate(
             input_ids = input_ids,
             # logits_processor=logits_processor,
-            num_return_sequences = 10,
+            num_return_sequences = 20,
             do_sample = True,
             # stopping_criteria=stopping_criteria,
             max_new_tokens = 100,
@@ -55,7 +55,7 @@ def test():
         outputs_sample = model.generate(
             input_ids = input_ids,
             # logits_processor=logits_processor,
-            num_return_sequences = 10,
+            num_return_sequences = 20,
             do_sample = True,
             # stopping_criteria=stopping_criteria,
             num_beams = 1,
